0f14445f4bee2ded443544332542e81d1b166175
JuanitaMG <juanitamartinezgutierrez9@gmail.com>
2026-07-06 23:37:57 -0500

    Mejora responsive y menú hamburguesa

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Portafolio iniciado 🚀");
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+document.querySelectorAll(".menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+});
